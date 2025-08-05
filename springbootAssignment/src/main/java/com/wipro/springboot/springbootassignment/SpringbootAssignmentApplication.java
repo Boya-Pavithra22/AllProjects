923a822c02736eb1ae98131e869302305dec6dd9
anilmanne <anilmanne@localhost>
2025-08-05 21:02:45 +0530

    Added spring boot

---
+++ springbootAssignment/src/main/java/com/wipro/springboot/springbootassignment/SpringbootAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.springboot.springbootassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.wipro.springboot")
+public class SpringbootAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAssignmentApplication.class, args);
+	}
+
+}
